3dcdd2ee079be36a8bf42ccbe0f59ff0edcd93ea
josephsaluta11 <91950106+josephsaluta11@users.noreply.github.com>
2021-10-09 13:16:19 +0800

    "Added new line, introduced myself as Joseph"

--- src/reyes/Main.java
+++ src/reyes/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Jefferson Reyes");
+        System.out.println("Hello Jefferson Reyes, my name is Joseph Saluta");
     }
 }
